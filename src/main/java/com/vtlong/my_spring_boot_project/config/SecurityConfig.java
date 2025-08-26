7ce7bcf4c7f2644804d61dd18486bf2a508ece1c
vietlxng <vietlong.eee@gmail.com>
2025-08-26 11:02:11 +0700

    update security configuration to support JWT authentication and add public endpoints

--- src/main/java/com/vtlong/my_spring_boot_project/config/SecurityConfig.java
+++ src/main/java/com/vtlong/my_spring_boot_project/config/SecurityConfig.java
@@ -1,15 +1,51 @@
 package com.vtlong.my_spring_boot_project.config;
 
+
+import javax.crypto.spec.SecretKeySpec;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.jose.jws.MacAlgorithm;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
 
 @Configuration
 public class SecurityConfig {
 
+    private final JwtConfig jwtConfig;
+
+    public SecurityConfig(JwtConfig jwtConfig) {
+        this.jwtConfig = jwtConfig;
+    }
+
+    private final String[] PUBLIC_ENDPOINTS = {
+        "/auth/login",
+        "/auth/introspect"
+    };
+
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.csrf(csrf -> csrf.disable());
+        httpSecurity.authorizeHttpRequests(request -> request.requestMatchers(HttpMethod.POST, PUBLIC_ENDPOINTS).permitAll()
+                .anyRequest().authenticated());
+        httpSecurity.oauth2ResourceServer(oauth2 -> 
+        oauth2.jwt(jwtConfigurer -> jwtConfigurer.decoder(jwtDecoder())));
+        return httpSecurity.build();
+    }
+
+    @Bean
+    public JwtDecoder jwtDecoder() {
+        SecretKeySpec secretKeySpec = new SecretKeySpec(jwtConfig.getSignerKey().getBytes(), "HS512");
+        return NimbusJwtDecoder.withSecretKey(secretKeySpec).macAlgorithm(MacAlgorithm.HS512).build();
     }
 }
